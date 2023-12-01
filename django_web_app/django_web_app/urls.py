203d397523dabf9993fc5c1b576f5caeba994c62
WonHoy <150685871+WonHoy@users.noreply.github.com>
2023-12-01 20:54:38 +0300

    Update urls.py
    a?

--- django_web_app/django_web_app/urls.py
+++ django_web_app/django_web_app/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('lug/', user_views.lug),
     path('', include('blog.urls')),
 ]
 
