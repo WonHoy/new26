4015d9f31430f069c2079d9d6a7333466ecbcd3a
WonHoy <150685871+WonHoy@users.noreply.github.com>
2023-12-01 22:11:17 +0300

    Update views.py

--- django_web_app/django_web_app/views.py
+++ django_web_app/django_web_app/views.py
@@ -1,4 +1,7 @@
 from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import ast
@@ -6,14 +9,11 @@ import datetime as dt
 from .py5110lug import *
 import math
 from .forms import *
-# TODO: Need to think what should we do with main index page. Create separate app?
-#def index(request):
-#    template = 'pt_applications/pin.html'
-#    return render(request, template)
+
 
 @csrf_exempt
 def lug(request):
-    template = '5110lug/index5110lug.html'
+    template = 'users/index5110.html'
     if request.method == "POST":
         submitbutton= request.POST.get("Submit")
 #Material        
@@ -110,76 +110,3 @@ def lug(request):
         except FileNotFoundError:
             pass
     return render(request, template, context)
-    
-def new13(request):
-    if request.method == "POST":
-        submitbutton= request.POST.get("Submit")
-        form= Form13(request.POST)
-        tab01="Start"
-        tab02="of"
-        if form.is_valid():
-            tab01= form.cleaned_data.get("tab01")
-            tab02= form.cleaned_data.get("tab02")
-        tab01=tab01+"Start"
-        tab02=tab02+"of"
-        context={ "tab01": tab01, "tab02": tab02, "tab03": tab03, "tab04": tab04}
-        try:
-            f = open('.\\new13.txt', 'a')
-            f.write(str(submitbutton)+"\n")
-            f.write(str(request)+"\n")
-            f.write(str(request.POST)+"\n")
-            f.write(str(context)+"\n")
-            f.write(str(dt.datetime.today())[0:19]+"\n")
-            f.close()
-        except FileNotFoundError:
-            pass
-    else:
-        context={ "tab01": "?Start", "tab02": "?of", "tab03": "?table", "tab04": "?!"}
-    return render(request, "5013new13/index5013new13.html", context)
-
-def new14(request):
-    if request.method == "POST":
-        submitbutton= request.POST.get("Submit")
-        form= Form14(request.POST)
-        name14a=""
-        name14b=""
-        name14c=""
-        name14d=""
-        name14e=""
-        name14f=""
-        if form.is_valid():
-            name14a= form.cleaned_data.get("name14a")
-            name14b= form.cleaned_data.get("name14b")
-            name14c= form.cleaned_data.get("name14c")
-            name14d= form.cleaned_data.get("name14d")
-            name14e= form.cleaned_data.get("name14e")
-            name14f= form.cleaned_data.get("name14f")
-        context={'form': form}
-        try:
-            f = open('.\\new14.txt', 'a')
-            f.write(str(submitbutton)+"\n")
-            f.write(str(request)+"\n")
-            f.write(str(request.POST)+"\n")
-            f.write(str(context)+"\n")
-            f.write(str(dt.datetime.today())[0:19]+"\n")
-            f.close()
-        except FileNotFoundError:
-            pass
-    else:
-        form= Form14()
-        context={'form': form, "name14b": "1111", "name14d": "22222", "name14f": "33333333333"}
-    return render(request, "5014new14/index5014new14.html", context)
-
-@csrf_exempt
-def lug_calc(request):
-    if request.method == 'POST':
-       data_input =ast.literal_eval(request.body.decode('utf-8') )
-       try:
-          f = open('.\\5110lug\\LOG\\log_5110lug.txt', 'a')
-          f.write(str(data_input)+"\n")
-          data_output=py5110lug_calc(data_input)
-          f.write(str(data_output)+"\n")
-          f.close()
-       except FileNotFoundError:
-              pass
-       return JsonResponse(data_output)
